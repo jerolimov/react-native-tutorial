f2330e895bec603695c6f6026c3109d59125517a
Christoph Jerolimov <jerolimov+git+2015@gmail.com>
2016-01-27 16:49:47 +0100

    Simple ListView example.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-import React, { Component, View, Text } from 'react-native';
+import React, { Component, View, Text, TouchableOpacity, ListView } from 'react-native';
 
-export default class HelloWorld extends Component {
+import cities from './cities';
+
+export default class ListViewExample extends Component {
+	constructor(props) {
+		super(props);
+
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			dataSource: ds.cloneWithRows(cities)
+		}
+	}
+
 	render() {
 		return (
-			<View style={{ margin: 50, padding: 50, backgroundColor: 'lightgray' }}>
-				<Text>Hello world!!!</Text>
+			<View style={{ flex: 1, paddingTop: 20 }}>
+				<ListView
+						dataSource={ this.state.dataSource }
+						renderRow={ this.renderRow.bind(this) }
+						style={{  }}
+				/>
 			</View>
 		)
+	}
+
+	renderRow(rowData) {
+		return (
+			<View style={{ padding: 10, borderColor: '#eeeeee', borderBottomWidth: 1 }}>
+				<TouchableOpacity>
+					<Text>{rowData}</Text>
+				</TouchableOpacity>
+			</View>
+		);
 	}
 }
