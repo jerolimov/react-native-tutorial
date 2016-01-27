a9db13e0f89bcd878bae42ac59d94ec6892671e9
Christoph Jerolimov <jerolimov+git+2015@gmail.com>
2016-01-27 15:55:09 +0100

    Create hello world component.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import React, { Component, View, Text } from 'react-native';
+
+export default class HelloWorld extends Component {
+	render() {
+		return (
+			<View style={{ margin: 50, padding: 50, backgroundColor: 'lightgray' }}>
+				<Text>Hello world!!!</Text>
+			</View>
+		)
+	}
+}
