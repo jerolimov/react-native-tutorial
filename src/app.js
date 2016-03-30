205ecb31a1d79868a3cca316e4a807b419c04953
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2016-03-30 22:31:14 +0200

    ListView with Sections example

--- src/app.js
+++ src/app.js
@@ -5,20 +5,47 @@ import cities from './cities';
 
 const styles = StyleSheet.create({
 	container: {
-		paddingTop: 20,
+		marginTop: 20,
 		flex: 1,
-		backgroundColor: 'gray'
+	},
+	section: {
+		padding: 10,
+		fontSize: 18,
+		fontWeight: 'bold',
+		backgroundColor: 'lightgray'
 	},
 	city: {
-		margin: 10,
-		fontSize: 18,
-		fontWeight: 'bold'
+		margin: 10
 	}
 });
 
-cities[0] = 'Vielvielvielvielvielvielvielvielzulangerstädtename';
+cities[0] = 'A Vielvielvielvielvielvielvielvielzulangerstädtename';
+
+const sections = {};
+const mapping = {
+	'Ä': 'A',
+	'Ö': 'O',
+	'Ü': 'U',
+	'Ś': 'S'
+}
+
+// Prepare sections A-Z
+for (var i = 65; i < 91; i++) {
+	sections[String.fromCharCode(i)] = [];
+	console.log();
+}
+
+// Map cities into sections
+cities.forEach((city) => {
+	let firstChar = city.substr(0, 1).toUpperCase();
+	if (mapping[firstChar]) {
+		firstChar = mapping[firstChar];
+	}
+	sections[firstChar].push(city);
+});
 
 const cityDataSource = new ListView.DataSource({
+	sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
 	rowHasChanged: (r1, r2) => r1 !== r2
 });
 
@@ -26,7 +53,7 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			dataSource: cityDataSource.cloneWithRows(cities)
+			dataSource: cityDataSource.cloneWithRowsAndSections(sections)
 		};
 	}
 
@@ -34,10 +61,15 @@ export default class App extends Component {
 		return (
 			<ListView
 					dataSource={ this.state.dataSource }
+					renderSectionHeader={ this.renderSectionHeader }
 					renderRow={ this.renderRow }
 					style={ styles.container }
 			/>
 		)
+	}
+
+	renderSectionHeader(sectionData, sectionID) {
+		return <Text style={ styles.section }>{ sectionID } ({ sectionData.length } cities)</Text>;
 	}
 
 	renderRow(rowData, sectionID, rowID, highlightRow) {
