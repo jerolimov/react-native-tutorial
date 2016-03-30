d3a709eda93da3a9b8ef4a057ac541bab44da272
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2016-03-30 22:32:37 +0200

    View Animations

--- src/app.js
+++ src/app.js
@@ -1,31 +1,65 @@
 
-import React, { Alert, Component, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { Animated, Component, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		paddingTop: 20,
+		alignItems: 'center',
 		justifyContent: 'center',
-		backgroundColor: '#eee'
+		backgroundColor: 'gray'
 	},
 	button: {
-		margin: 16,
-		padding: 16,
-		backgroundColor: '#53c5ff'
+		position: 'absolute',
+		left: 16,
+		right: 16,
+		bottom: 16,
+		padding: 10,
+		borderWidth: 2,
+		borderColor: 'white',
+		borderRadius: 10
 	},
 	text: {
-		textAlign: 'center',
-		fontSize: 22,
+		fontSize: 18,
 		fontWeight: 'bold',
-		color: 'white'
+		color: 'white',
+		backgroundColor: 'transparent'
 	}
 });
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			boxWidth: new Animated.Value(200),
+			boxHeight: new Animated.Value(100)
+		};
+	}
+
+	toggle() {
+		Animated.parallel([
+			Animated.spring(this.state.boxWidth, {
+				toValue: this.state.boxWidth._value !== 200 ? 200 : 100
+			}),
+			Animated.spring(this.state.boxHeight, {
+				toValue: this.state.boxHeight._value !== 100 ? 100 : 200
+			})
+		]).start();
+	}
+
 	render() {
+		const width = this.state.boxWidth;
+		const height = this.state.boxHeight;
+
 		return (
 			<View style={ styles.container }>
-				<TouchableOpacity onPress={ () => Alert.alert('Button pressed!') } style={ styles.button }>
-					<Text style={ styles.text }>Hello world !!!</Text>
+
+				<Animated.View style={{ width, height, backgroundColor: 'lightgray' }}>
+					<Text style={ styles.text }>{ width._value } x { height._value }</Text>
+				</Animated.View>
+
+				<TouchableOpacity style={ styles.button } onPress={ this.toggle.bind(this) }>
+					<Text style={ styles.text }>Toggle</Text>
 				</TouchableOpacity>
 			</View>
 		);
